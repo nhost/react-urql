7e7c862876d00a2ae697edc50d9db7c98958f18a
Cezar Neaga <hi@cezarneaga.eu>
2020-10-11 22:32:34 +0200

    first rough draft

--- src/index.js
+++ src/index.js
@@ -1,197 +1,164 @@
-import React, { createContext, useContext } from "react";
-import { InMemoryCache } from "apollo-cache-inmemory";
-import { ApolloProvider, ApolloClient } from "@apollo/client";
-import { setContext } from "@apollo/link-context";
-import { WebSocketLink } from "apollo-link-ws";
-import { createHttpLink } from "apollo-link-http";
-import { from, split } from "apollo-link";
-import { getMainDefinition } from "apollo-utilities";
+import React, { createContext, useContext, useState, useEffect } from 'react'
+import fetch from 'isomorphic-unfetch'
+// import regeneratorRuntime from 'regenerator-runtime'
+import ws from 'isomorphic-ws'
+import { authExchange } from '@urql/exchange-auth'
+import {
+  dedupExchange,
+  // debugExchange,
+  cacheExchange,
+  fetchExchange,
+  subscriptionExchange,
+  Provider,
+  createClient,
+} from 'urql'
+import { SubscriptionClient } from 'subscriptions-transport-ws'
 
-export function generateApolloClient(
+export function generateUrqlClient(
   auth,
   gqlEndpoint,
-  headers,
-  publicRole = "public"
+  // headers, //do we still need them? see addAuthToOperation()
+  publicRole = 'public'
 ) {
-  const getheaders = (auth) => {
-    // add headers
-    const resHeaders = {
-      ...headers,
-    };
-
-    // add auth headers if signed in
-    // or add 'public' role if not signed in
-    if (auth) {
-      if (auth.isAuthenticated()) {
-        resHeaders.authorization = `Bearer ${auth.getJWTToken()}`;
-      } else {
-        resHeaders.role = publicRole;
+  const ssr = typeof window === 'undefined'
+  const getAuth = () => {
+    if (!auth.isAuthenticated()) {
+      const token = auth.getJWTToken()
+      const refreshToken = !ssr && localStorage.getItem('refresh_token')
+      if (token && refreshToken) {
+        console.log('getAuth -> token', token)
+        console.log('getAuth -> refreshToken', refreshToken)
+        return { token, refreshToken }
       }
+      return null
     }
+    // we could try a refresh token mutation/operation
+    // if auth.refreshToken() would be a public function
 
-    return resHeaders;
-  };
+    // const result = await auth.refreshToken(authState.refreshToken)
+    // if (result) {
+    //   return
+    // }
+    // else we logout
+    // auth.logout()
 
-  const ssr = typeof window === "undefined";
-  const uri = gqlEndpoint;
-
-  const wsUri = uri.startsWith("https")
-    ? uri.replace(/^https/, "wss")
-    : uri.replace(/^http/, "ws");
-
-  const wsLink = !ssr
-    ? new WebSocketLink({
-        uri: wsUri,
-        options: {
-          reconnect: true,
-          lazy: true,
-          connectionParams: () => {
-            const connectionHeaders = getheaders(auth);
-            return {
-              headers: connectionHeaders,
-            };
-          },
-        },
-      })
-    : null;
-
-  const httplink = createHttpLink({
-    uri,
-  });
-
-  const authLink = setContext((a, { headers }) => {
+    return null
+  }
+  const addAuthToOperation = ({ operation }) => {
+    const fetchOptions =
+      typeof operation.context.fetchOptions === 'function'
+        ? operation.context.fetchOptions()
+        : operation.context.fetchOptions || {}
     return {
-      headers: {
-        ...headers,
-        ...getheaders(auth),
-      },
-    };
-  });
-
-  const link = !ssr
-    ? split(
-        ({ query }) => {
-          const { kind, operation } = getMainDefinition(query);
-          return kind === "OperationDefinition" && operation === "subscription";
+      ...operation,
+      context: {
+        ...operation.context,
+        fetchOptions: {
+          ...fetchOptions,
+          headers: !auth.isAuthenticated()
+            ? {
+                ...fetchOptions.headers,
+                role: publicRole,
+              }
+            : {
+                ...fetchOptions.headers,
+                Authorization: `Bearer ${auth.getJWTToken()}`,
+              },
         },
-        wsLink,
-        authLink.concat(httplink)
-      )
-    : httplink;
+      },
+    }
+  }
+  const didAuthError = ({ error }) => {
+    return error.graphQLErrors.some((e) => e.extensions?.code === 'FORBIDDEN')
+  }
+  // const ssr = typeof window === 'undefined'
+  const uri = gqlEndpoint
 
-  const client = new ApolloClient({
-    ssr: ssr,
-    link: from([link]),
-    cache: new InMemoryCache(),
-    defaultOptions: {
-      watchQuery: {
-        fetchPolicy: "cache-and-network",
+  const wsUri = uri.startsWith('https')
+    ? uri.replace(/^https/, 'wss')
+    : uri.replace(/^http/, 'ws')
+
+  const subscriptionClient = new SubscriptionClient(
+    wsUri,
+    {
+      reconnect: true,
+      connectionParams: {
+        headers: !auth.isAuthenticated()
+          ? { role: publicRole }
+          : { Authorization: `Bearer ${auth.getJWTToken()}` },
       },
     },
-  });
+    ws
+  )
 
-  return { client, wsLink };
+  const client = createClient({
+    url: uri,
+    fetch,
+    requestPolicy: 'cache-and-network',
+    exchanges: [
+      dedupExchange,
+      // debugExchange,
+      cacheExchange,
+      authExchange({
+        getAuth,
+        addAuthToOperation,
+        didAuthError,
+      }),
+      fetchExchange,
+      subscriptionExchange({
+        forwardSubscription(operation) {
+          return subscriptionClient.request(operation)
+        },
+      }),
+    ],
+  })
+
+  return client
 }
 
-export class NhostApolloProvider extends React.Component {
-  constructor(props) {
-    super(props);
+export function NhostUrqlProvider(props) {
+  const { auth, gqlEndpoint, publicRole = 'public', children } = props
+  const client = generateUrqlClient(auth, gqlEndpoint, publicRole)
+  // i think this is no longer needed.
 
-    const { auth, gqlEndpoint, headers, publicRole = "public" } = this.props;
-    const { client, wsLink } = generateApolloClient(
-      auth,
-      gqlEndpoint,
-      headers,
-      publicRole
-    );
-    this.client = client;
-    this.wsLink = wsLink;
+  //   if (props.auth) {
+  //     this.props.auth.onTokenChanged(() => {
+  //       if (this.wsLink.subscriptionClient.status === 1) {
+  //         this.wsLink.subscriptionClient.tryReconnect()
+  //       }
+  //     })
 
-    // for debug
-    // const ssr = typeof window === "undefined";
-    // if (!ssr) {
-    //   console.log(" not ssr, set up .on");
-    //   this.wsLink.subscriptionClient.on("connecting", () =>
-    //     console.log("connecting")
-    //   );
-    //   this.wsLink.subscriptionClient.on("connected", () =>
-    //     console.log("connected")
-    //   );
-    //   this.wsLink.subscriptionClient.on("reconnecting", () =>
-    //     console.log("reconnecting")
-    //   );
-    //   this.wsLink.subscriptionClient.on("reconnected", () =>
-    //     console.log("reconnected")
-    //   );
-    //   this.wsLink.subscriptionClient.on("disconnected", () =>
-    //     console.log("disconnected")
-    //   );
-    //   this.wsLink.subscriptionClient.on("error", () => console.log("error"));
-    // }
+  //     this.props.auth.onAuthStateChanged((data) => {
+  //       // reconnect ws connection with new auth headers for the logged in/out user
+  //       if (this.wsLink.subscriptionClient.status === 1) {
+  //         // must close first to avoid race conditions
+  //         this.wsLink.subscriptionClient.close()
+  //         // reconnect
+  //         this.wsLink.subscriptionClient.tryReconnect()
+  //       }
+  //     })
+  //   }
+  // }
 
-    if (this.props.auth) {
-      this.props.auth.onTokenChanged(() => {
-        if (this.wsLink.subscriptionClient.status === 1) {
-          this.wsLink.subscriptionClient.tryReconnect();
-        }
-      });
-
-      this.props.auth.onAuthStateChanged((data) => {
-        // reconnect ws connection with new auth headers for the logged in/out user
-        if (this.wsLink.subscriptionClient.status === 1) {
-          // must close first to avoid race conditions
-          this.wsLink.subscriptionClient.close();
-          // reconnect
-          this.wsLink.subscriptionClient.tryReconnect();
-        }
-      });
-    }
-  }
-
-  render() {
-    return (
-      <ApolloProvider client={this.client}>
-        {this.props.children}
-      </ApolloProvider>
-    );
-  }
+  return <Provider value={client}>{children}</Provider>
 }
 
-export const AuthContext = createContext({ signedIn: null });
+export const AuthContext = createContext({ signedIn: null })
 
-export class NhostAuthProvider extends React.Component {
-  constructor(props) {
-    super(props);
+export function NhostAuthProvider({ auth, children }) {
+  const [signedIn, setSignedIn] = useState(auth.isAuthenticated())
+  useEffect(() => {
+    auth.onAuthStateChanged((data) => {
+      setSignedIn(data)
+    })
+  }, [auth.onAuthStateChanged])
 
-    this.state = {
-      signedIn: props.auth.isAuthenticated(),
-    };
-
-    props.auth.onAuthStateChanged((data) => {
-      if (this.is_mounted) {
-        this.setState({ signedIn: data });
-      }
-    });
-  }
-
-  componentDidMount() {
-    this.is_mounted = true;
-  }
-
-  componentWillUnmount() {
-    this.is_mounted = false;
-  }
-
-  render() {
-    return (
-      <AuthContext.Provider value={{ signedIn: this.state.signedIn }}>
-        {this.props.children}
-      </AuthContext.Provider>
-    );
-  }
+  return (
+    <AuthContext.Provider value={{ signedIn }}>{children}</AuthContext.Provider>
+  )
 }
 
 export function useAuth() {
-  const context = useContext(AuthContext);
-  return context;
+  const context = useContext(AuthContext)
+  return context
 }
